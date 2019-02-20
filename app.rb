9e88ccc73cbc2f81a2cca984df82a78842b26427
ch2isk4kos <ch2isk4kos@gmail.com>
2019-02-20 15:55:44 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    # configure do
+    #     set :public_folder, 'public'
+    #     set :views, 'app/views'
+    # end
+
+    # homepage route
+    get '/' do
+        erb :welcome
+    end
+
+    # name route
+    get '/name' do
+        erb :name
+    end
+
+    # hometown route
+    get '/hometown' do
+        erb :hometown
+    end
+
+    # favorite-song route
+    get '/favorite-song' do
+        erb :favorite_song
+    end
+
 end
