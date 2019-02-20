3656b34ba6d9cd53a966a632906c2744015b3df7
Nate <github email address>
2019-02-19 19:59:15 -0500

    tests passed

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Nate"
+  end
+
+  get '/hometown' do
+    "My hometown is Stockton, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is anything by Frank Sinatra."
+  end
+
 end
